dbb132af8a41fb826debffc424de46b0f88d13f5
Anand Satya Vetcha <AnandVetcha@users.noreply.github.com>
2016-02-20 22:00:25 +0530

    Update and rename Blink.py to DigitalWrite.py

--- DigitalWrite.py
+++ DigitalWrite.py
@@ -2,16 +2,18 @@ import time
 import mraa
 
 ###### Programe Begins ####
-print (mraa.getVersion())
-Led = mraa.Gpio(4)
+print (mraa.getVersion())  #Print mraa version for reference
+
+# Initiate Digital Pin 4 with direction out
+Led = mraa.Gpio(4)    
 Led.dir(mraa.DIR_OUT)
-Led.write(1)
+
 value = 0
 while 1:
         if value == 0:
                 value = 1
         else:
                 value = 0
-        Led.write(value)
+        Led.write(value)   #Set voltage at digital pin to HIGH/LOW
         print "LED "+str(value)
         time.sleep(1)
